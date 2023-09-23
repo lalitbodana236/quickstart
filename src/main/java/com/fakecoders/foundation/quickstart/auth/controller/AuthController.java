edf4afd085004ad5ccb89776b9fa443dc1593789
lalitbodana236 <lalitbodana236@gmail.com>
2023-09-23 21:33:15 +0530

    fixed

--- src/main/java/com/fakecoders/foundation/quickstart/auth/controller/AuthController.java
+++ src/main/java/com/fakecoders/foundation/quickstart/auth/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,6 +52,7 @@ import java.util.Optional;
 @RequestMapping("/api/auth")
 @Tag(name = "Authorization Rest API", description = "Defines endpoints that can be hit only when the user is not " +
         "logged in. It's not secured by default.")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
